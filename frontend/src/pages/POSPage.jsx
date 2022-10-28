74637ffd6c340a61a1d2507b90b0eb76545c46d9
Aravind Roy <aravindroy1709@gmail.com>
2022-10-28 13:22:56 +0530

    Rendered Products

--- frontend/src/pages/POSPage.jsx
+++ frontend/src/pages/POSPage.jsx
@@ -1,13 +1,47 @@
-import {React,useEffect} from 'react'
+import {React,useEffect , useState} from 'react'
 import { Link } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
-
+import Axios from 'axios'
 const POSPage = () => {
 
+  const [product , setProduct] =useState([])
+  const [isLoading , setIsLoading] = useState(false)
   
+  const fetchProducts = async() => {
+    setIsLoading(true)
+    const result = await Axios.get('http://localhost:5000/products')
+    .then((res) => {
+      setProduct(res.data)
+    })
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  },[])
+  
+  const AddProduct = (product)=>{
+  console.log(product)
+  }
   return (
+   
     <MainLayout>
-       <div>Pos Page</div>
+      <>
+      <div className='row'>
+        <div className='col-md-8'>
+          {isLoading ? 'Loading' : <div className='row'>
+          {product.map((product,key)=>
+          <div key={key} className='col-md-4'>
+            <div className='border' onClick={()=>{AddProduct(product)}}>
+              <p>{product.name}</p>
+              <img src={product.image} className = "img-fluid" alt={product.name} />
+            </div>
+          </div>
+        )}  
+        </div>}
+        </div>
+      </div>
+      </>
 
     </MainLayout>
    
