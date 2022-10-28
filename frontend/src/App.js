d537a3f2b256c7adb3cd00aa6835b13267d2f9b8
Aravind Roy <aravindroy1709@gmail.com>
2022-10-28 12:13:41 +0530

    Routes Defined

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import HomePage from "./pages/HomePage"
 import POSPage from "./pages/POSPage"
-
+import {BrowserRouter as Router , Routes ,Route} from 'react-router-dom'
 function App() {
   return (
    <>
-      <HomePage />
-      <POSPage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/pos" element={<POSPage />} />
+      </Routes>
+    </Router>
    </>
   )
 }
